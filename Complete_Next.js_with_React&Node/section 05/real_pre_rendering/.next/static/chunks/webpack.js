884c3257ce7651058efc3942ac33ccb4e877fd24
yangsnagwoo <dndb3599@gmail.com>
2022-08-14 15:29:57 +0900

    feat : section 112

--- Complete_Next.js_with_React&Node/section 05/real_pre_rendering/.next/static/chunks/webpack.js
+++ Complete_Next.js_with_React&Node/section 05/real_pre_rendering/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b564e06122b965925473";
+/******/ 	var hotCurrentHash = "3a444325773c3cc93b5b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- Complete_Next.js_with_React&Node/section 05/real_pre_rendering/pages/last-sales.js
+++ Complete_Next.js_with_React&Node/section 05/real_pre_rendering/pages/last-sales.js
@@ -1,41 +1,67 @@
 import { useEffect, useState } from "react";
-import { useSWR } from "swr";
+import useSWR from "swr";
 export default function LastSalesPage(props) {
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
   const [sales, setSales] = useState();
 
+  // 컴포넌트가 로드 될때 url에 대한 요청을 보내게 된다
+
+  // return 값을 받는다
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const { data, error } = useSWR(
+    `https://nextjs-prerender-d2499-default-rtdb.firebaseio.com/sales.json`,
+    fetcher
+  );
+
   useEffect(() => {
-    setIsLoading(true);
-
-    fetch(
-      "https://nextjs-prerender-d2499-default-rtdb.firebaseio.com/sales.json"
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        const transformedSales = Object.keys(data).map((key) => {
-          return {
-            ...data[key],
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          };
-        });
-
-        console.log(transformedSales);
-        setSales(transformedSales);
-        setIsLoading(false);
+    if (data) {
+      const transformedSales = Object.keys(data).map((key) => {
+        return {
+          ...data[key],
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        };
       });
-  }, []);
+      setSales(transformedSales);
+    }
+  }, [data]);
 
-  if (isLoading) {
-    return <p>Loading...</p>;
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  //   fetch(
+  //     "https://nextjs-prerender-d2499-default-rtdb.firebaseio.com/sales.json"
+  //   )
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       console.log(data);
+  //       const transformedSales = Object.keys(data).map((key) => {
+  //         return {
+  //           ...data[key],
+  //           id: key,
+  //           username: data[key].username,
+  //           volume: data[key].volume,
+  //         };
+  //       });
+
+  //       console.log(transformedSales);
+  //       setSales(transformedSales);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  // 로딩보다 에러가 더 중요하기 때문에 앞서 두자
+  if (error) {
+    return <p>No sales yet</p>;
   }
 
-  if (!sales) {
-    return <p>No sales yet</p>;
+  if (!data || !sales) {
+    return <p>Loading...</p>;
   }
 
   return (
