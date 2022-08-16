01673aab580921656dbc4bbd914bb895b6acb239
yangsnagwoo <dndb3599@gmail.com>
2022-08-16 20:03:37 +0900

    finish : scetion 07

--- Complete_Next.js_with_React&Node/section 07/pages/_app.js
+++ Complete_Next.js_with_React&Node/section 07/pages/_app.js
@@ -1,9 +1,13 @@
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import Layout from "../components/layout/layout";
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta name="description" content="This is a description" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
