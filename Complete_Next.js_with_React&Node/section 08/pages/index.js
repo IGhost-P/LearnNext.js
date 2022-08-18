b11ead24a219ed822684cad4ce207567939b6215
yangsnagwoo <dndb3599@gmail.com>
2022-08-18 10:36:06 +0900

    feat : make logic for backend api

--- Complete_Next.js_with_React&Node/section 08/pages/index.js
+++ Complete_Next.js_with_React&Node/section 08/pages/index.js
@@ -1,7 +1,36 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailInput = useRef(null);
+  const passwordInput = useRef(null);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    const enteredEmail = emailInput.current.value;
+    const enteredPassword = passwordInput.current.value;
+
+    if (enteredEmail.trim() === "" || enteredPassword.trim() === "") {
+      return alert("Please enter your email and password");
+    }
+
+    // typing here : send request to server
+  };
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitForm}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInput} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows="5" ref={passwordInput} />
+        </div>
+        <button type="submit">Send Feedback</button>
+      </form>
     </div>
   );
 }
